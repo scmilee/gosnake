38703293eaf4b91b0264cb81daba3cd98e7a9749
handsome-max <heykeroth.dev@gmail.com>
2019-01-23 01:12:55 +0100

    [ADD] own body collision for snake, game_states, endgame_screen

--- gosnake.go
+++ gosnake.go
@@ -8,17 +8,27 @@ import (
 	"time"
 )
 
-const table_height = 50
-const table_width = 50
-const FPS = 5
+//make this variable
+const table_height = 30
+const table_width = 30
+const FPS = 7
+const body_gain = 3
 const fruit_spawn_timer = 4000
-
 const head_char = 'O'
 const body_char = 'o'
 const wall_char = 'X'
 const fruit_char = '0'
 
-const game_title = "GO-SNAKE GAME"
+const game_title = "GO-SNAKE"
+
+const (
+	easy       int8 = 1
+	medium     int8 = 2
+	hard       int8 = 3
+	impossible int8 = 4
+)
+
+var game_difficulty int8 = easy
 
 // enums for snake movement
 /*
@@ -33,7 +43,21 @@ const (
 	left  int8 = 3
 )
 
-var pause = false
+// game state indicates the current state of the game
+//	-1 -> game aborted/closed
+//	0	-> game not started
+//	1	-> game started
+//	2	-> game paused
+//	3	-> game lost
+const (
+	st_closed      int8 = -2
+	st_not_started int8 = 0
+	st_running     int8 = 1
+	st_paused      int8 = -1
+	st_lost        int8 = -3
+)
+
+var game_state int8 = st_not_started
 
 type Point struct {
 	//TODO: make this float, so its easier to scale with speeds and FPS
@@ -53,10 +77,12 @@ type Snake struct {
 	//speed is a value between 0 and 100
 	speed     uint8
 	direction int8
+	score     int
 }
 
 func (s *Snake) resetSnake() {
 	s.speed = 1
+	s.score = 0
 	s.direction = right
 	s.body = append(s.body, Point{table_height / 2, table_width / 2})
 }
@@ -89,8 +115,30 @@ func (s *Snake) move() {
 	}
 }
 
-func (s *Snake) addToBody(p Point) {
-	s.body = append(s.body, p)
+func (s *Snake) equalPoints(p *Point) bool {
+	if (*s).body[0].x == p.x && (*s).body[0].y == p.y {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (s *Snake) checkCollisionBody() bool {
+	if len(s.body) > 2 {
+		for _, pos := range s.body[2:] {
+			// collision with own body
+			if s.equalPoints(&pos) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (s *Snake) addToBody(p Point, numb int) {
+	for i := 0; i < numb; i++ {
+		s.body = append(s.body, p)
+	}
 }
 
 // Check if the given dir is a valid direction to move
@@ -126,16 +174,16 @@ func (s Snake) checkMove(dir int8) bool {
 
 func (s Snake) checkCollisionWall() bool {
 	switch {
-	case s.body[0].x >= table_height-1:
-		return false
-	case s.body[0].x <= 0:
-		return false
-	case s.body[0].y >= table_width-1:
-		return false
-	case s.body[0].y <= 0:
-		return false
-	default:
+	case s.body[0].y >= table_height-1:
 		return true
+	case s.body[0].y <= 0:
+		return true
+	case s.body[0].x >= table_width-1:
+		return true
+	case s.body[0].x <= 0:
+		return true
+	default:
+		return false
 	}
 }
 
@@ -143,7 +191,8 @@ func (s *Snake) checkCollisionFruit(f *Fruit) {
 	for i, pos := range *f {
 		if pos.x == s.body[0].x && pos.y == s.body[0].y {
 			*f = append((*f)[:i], (*f)[i+1:]...)
-			s.addToBody(s.body[len(s.body)-1])
+			s.addToBody(s.body[len(s.body)-1], body_gain)
+			s.score = s.score + (1 * int(game_difficulty))
 		}
 	}
 }
@@ -152,31 +201,31 @@ func (s *Snake) checkCollisionFruit(f *Fruit) {
 type Game_table [table_height][table_width]byte
 
 func (table *Game_table) updateGameTable(s Snake, fruit *Fruit) {
-	for x := range table {
-		for y := range table[x] {
+	for x := range *table {
+		for y := range (*table)[x] {
 			// left wall
 			if x == 0 {
-				table[x][y] = wall_char
+				(*table)[x][y] = wall_char
 				// right wall
 			} else if x == table_height-1 {
-				table[x][y] = wall_char
+				(*table)[x][y] = wall_char
 				// top wall
 			} else if y == 0 {
-				table[x][y] = wall_char
+				(*table)[x][y] = wall_char
 				// botoom wall
 			} else if y == table_width-1 {
-				table[x][y] = wall_char
+				(*table)[x][y] = wall_char
 				// fill rest with spaces
 				//TODO replace with current player and objects in the map
 			} else {
-				table[x][y] = ' '
+				(*table)[x][y] = ' '
 				for i := 1; i < len(s.body); i++ {
-					table[s.body[i].y][s.body[i].x] = body_char
+					(*table)[s.body[i].y][s.body[i].x] = body_char
 				}
 				for _, f := range *fruit {
-					table[f.y][f.x] = fruit_char
+					(*table)[f.y][f.x] = fruit_char
 				}
-				table[s.body[0].y][s.body[0].x] = head_char
+				(*table)[s.body[0].y][s.body[0].x] = head_char
 			}
 		}
 	}
@@ -189,14 +238,21 @@ func main() {
 		panic(err)
 	}
 	defer termbox.Close()
-	fmt.Printf("\t\t%v\n", game_title)
+	game_state = st_running
+	game_difficulty = easy
+
+	// seed random number generator
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	//init stuff
 	var table Game_table
-	table.resetGameTable()
-	table.drawTable()
 	var s Snake
 	var f Fruit
 	s.resetSnake()
+	table.resetGameTable()
+	f.spawnFruit()
+	table.drawTable(&s)
+
 	//Tick Intervall s = 600 msecs/ 30 FPS
 	ticker_fps := time.NewTicker(600 / FPS * time.Millisecond)
 	/*
@@ -215,61 +271,78 @@ func main() {
 
 mainloop:
 	for range ticker_fps.C {
-		select {
-		case dir := <-c:
-			switch dir {
-			//pause
-			case -2:
-				pause = !pause
-				update(&s, &table, &f)
-				//abort
-			case -1:
-				break mainloop
-				// up
-			case up:
-				if s.checkMove(dir) == true {
-					s.direction = dir
+		if game_state != st_lost {
+			select {
+			case dir := <-c:
+				switch dir {
+				//pause
+				case st_paused:
+					if game_state == st_paused {
+						game_state = st_running
+					} else if game_state == st_running {
+						game_state = st_paused
+
+					}
+					update(&s, &table, &f)
+					//abort
+				case st_closed:
+					break mainloop
+					// up
+				case up:
+					if s.checkMove(dir) == true {
+						s.direction = dir
+					}
+					update(&s, &table, &f)
+					// right
+				case right:
+					if s.checkMove(dir) == true {
+						s.direction = dir
+					}
+					update(&s, &table, &f)
+					// down
+				case down:
+					if s.checkMove(dir) == true {
+						s.direction = dir
+					}
+					update(&s, &table, &f)
+					//left
+				case left:
+					if s.checkMove(dir) == true {
+						s.direction = dir
+					}
+					update(&s, &table, &f)
 				}
+			case <-fruit_spawn:
+				f.spawnFruit()
 				update(&s, &table, &f)
-				// right
-			case right:
-				if s.checkMove(dir) == true {
-					s.direction = dir
-				}
-				update(&s, &table, &f)
-				// down
-			case down:
-				if s.checkMove(dir) == true {
-					s.direction = dir
-				}
-				update(&s, &table, &f)
-				//left
-			case left:
-				if s.checkMove(dir) == true {
-					s.direction = dir
-				}
+			default:
 				update(&s, &table, &f)
 			}
-		case <-fruit_spawn:
-			f.spawnFruit()
-			update(&s, &table, &f)
-		default:
-			update(&s, &table, &f)
+		} else {
+			break
 		}
 	}
 	ticker_fps.Stop()
+
+	fmt.Print(cursor.ClearEntireScreen())
+	fmt.Print(cursor.MoveTo(0, 0))
+	fmt.Printf("\n\n\t\tYOU LOST\n\t\t---------\n\t\tScore: %v\n\n", s.score)
+	fmt.Printf("\nPress any key to close the game...")
+	time.Sleep(10 * time.Millisecond)
+	termbox.PollEvent()
+
 }
 
 func update(s *Snake, table *Game_table, f *Fruit) {
-	if pause == false {
+	if game_state == st_running {
 		s.move()
 		//collision detection -> if false ==> collision detected
-		if !s.checkCollisionWall() {
-			pause = true
+		if s.checkCollisionWall() || s.checkCollisionBody() {
+			game_state = st_lost
 		}
 		s.checkCollisionFruit(f)
 		table.updateGameTable(*s, f)
-		table.drawTable()
+		table.drawTable(s)
 	}
 }
 
@@ -285,10 +358,10 @@ func checkKeyInput(c chan int8) {
 				c <- down
 			} else if ev.Key == termbox.KeyArrowLeft {
 				c <- left
-			} else if ev.Key == termbox.KeyCtrlP {
-				c <- -2
+			} else if ev.Ch == 'p' {
+				c <- st_paused
 			} else if ev.Key == termbox.KeyEsc {
-				c <- -1
+				c <- st_closed
 				break
 			}
 		}
@@ -303,10 +376,10 @@ func spawnFruitTicker(fruit_chan chan bool) {
 	fruit_ticker.Stop()
 }
 
-func (table Game_table) drawTable() {
+func (table Game_table) drawTable(s *Snake) {
 	fmt.Print(cursor.ClearEntireScreen())
 	fmt.Print(cursor.MoveTo(0, 0))
-	fmt.Printf("\t\t%v\n", game_title)
+	fmt.Printf("\t\t%v\nScore: %v\n", game_title, s.score)
 
 	for x := range table {
 		for y := range table[x] {
